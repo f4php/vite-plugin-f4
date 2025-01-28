a7ddc8cf6bf46b2f7b4aac1069c3b87813db8658
Dennis Kreminsky <dennis@kreminsky.com>
2025-01-28 21:33:39 +0200

    Fix missing semicolon

--- src/fix-import-analysis-plugin.js
+++ src/fix-import-analysis-plugin.js
@@ -3,7 +3,7 @@ function createFixImportAnalysisPlugin(options, config, oldPlugin) {
     name: 'vite-plugin-f4-fix-import-analysis',
     transform(code, id) {
       if(id.startsWith(options.prefix)) {
-        return code.replace(/import\s+\"(?!\/@)(\/.+)\";/g, `import "/@fs${config.root}$1"`);
+        return code.replace(/import\s+\"(?!\/@)(\/.+)\";/g, `import "/@fs${config.root}$1";`);
       }
     },
   };
