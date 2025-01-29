2ce392c8a01402314b1ccaaaf0fec8edc62fe860
Dennis Kreminsky <dennis@kreminsky.com>
2025-01-29 18:44:25 +0200

    Adjusted handling of HMR requests with strict proxy settings

--- src/fix-import-analysis-plugin.js
+++ src/fix-import-analysis-plugin.js
@@ -1,9 +1,21 @@
-function createFixImportAnalysisPlugin(options, config, oldPlugin) {
+function createFixImportAnalysisPlugin(options, config) {
   return {
     name: 'vite-plugin-f4-fix-import-analysis',
+    handleHotUpdate({ type, file, server }) {
+      if(type==='update' && file.startsWith(config.root)) {
+        const targetModule = server.moduleGraph.getModuleById(file);
+        targetModule.url = `/@fs${file}`;
+      }
+    },
     transform(code, id) {
       if(id.startsWith(options.prefix)) {
         return code.replace(/import\s+\"(?!\/@)(\/.+)\";/g, `import "/@fs${config.root}$1";`);
+      }
+      else if (id.startsWith(config.root) && !id.startsWith(config.root+'/node_modules')) {
+        const relativePath = id.slice(config.root.length);
+        const regexp = new RegExp(`__vite__createHotContext\\("${relativePath}"\\)`, "g");
+        const replacement = `__vite__createHotContext("/@fs${id}")`;
+        return code.replace(regexp, replacement)
       }
     },
   };
