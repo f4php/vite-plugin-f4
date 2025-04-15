48f944c8f588ead04bb51204c924e0241366ea2e
Dennis Kreminsky <dennis@kreminsky.com>
2025-04-15 12:11:46 +0300

    Limited import analysis transformation to dev mode of vite

--- src/fix-import-analysis-plugin.js
+++ src/fix-import-analysis-plugin.js
@@ -11,37 +11,39 @@ function createFixImportAnalysisPlugin(options, config) {
     },
     transform(code, id) {
       const root = `/${config.root.replace(/^\/+|\/+$/g, '')}`;
-      if(id.startsWith(options.prefix)) {
-        return code.replace(/import\s+(?<quote>([\"\'])?)(?!\/@)(?<path>\/\S+)\k<quote>/g, function(match, p1, p2, p3, offset, string, groups) {
-          return `import "/@fs${root}${groups.path}"`;
-        });
-      }
-      else if(id.replace(root, '').match(options.transformPathRegexp)) {
-        return code.replace(/import\s+(?<from>(.+?\s+from\s+))?(?<quote>([\"\'])?)(?<path>\S+?)\k<quote>/g, function(match, p1, p2, p3, p4, p5, offset, string, groups) {
-          if(groups.path.match(options.transformPathRegexp)) {
-            return groups.from ?
-              `import ${groups.from}"/@fs${root}${groups.path}";`
-              :
-              `import "/@fs${root}${groups.path}";`;
-          }
-          return match;
-        });
-      }
-      else if(id.match(options.stylesheetFilenameRegexp)) {
-        return code.replace(/url\((?<quote>([\"\'])?)(?<path>\S+?)\k<quote>\)/g, function(match, p1, p2, p3, offset, string, groups) {
-          if(groups.path.match(options.inlineAssetsUrlRegexp)) {
-            if(!groups.path.startsWith(root)) {
-              return `url('/@fs/${root}${groups.path}')`;
-            };
-          }
-          return match;
-        });
-      }
-      else if (id.startsWith(root) && !id.startsWith(`${root}/node_modules`)) {
-        const relativePath = id.slice(root.length);
-        const regexp = new RegExp(`__vite__createHotContext\\("${relativePath}"\\)`, "g");
-        const replacement = `__vite__createHotContext("/@fs${id}")`;
-        return code.replace(regexp, replacement)
+      if (config.command === 'serve') {
+        if(id.startsWith(options.prefix)) {
+          return code.replace(/import\s+(?<quote>([\"\']))(?!\/@)(?<path>\/.+?)\k<quote>;/g, function(match, p1, p2, p3, offset, string, groups) {
+            return `import "/@fs${root}${groups.path}";`;
+          });
+        }
+        else if(id.replace(root, '').match(options.transformPathRegexp)) {
+          return code.replace(/import\s+(?<from>(.+?\s+from\s+))?(?<quote>([\"\'])?)(?<path>\S+?)\k<quote>/g, function(match, p1, p2, p3, p4, p5, offset, string, groups) {
+            if(groups.path.match(options.transformPathRegexp)) {
+              return groups.from ?
+                `import ${groups.from}"/@fs${root}${groups.path}";`
+                :
+                `import "/@fs${root}${groups.path}";`;
+            }
+            return match;
+          });
+        }
+        else if(id.match(options.stylesheetFilenameRegexp)) {
+          return code.replace(/url\((?<quote>([\"\'])?)(?<path>\S+?)\k<quote>\)/g, function(match, p1, p2, p3, offset, string, groups) {
+            if(groups.path.match(options.inlineAssetsUrlRegexp)) {
+              if(!groups.path.startsWith(root)) {
+                return `url('/@fs/${root}${groups.path}')`;
+              };
+            }
+            return match;
+          });
+        }
+        else if (id.startsWith(root) && !id.startsWith(`${root}/node_modules`)) {
+          const relativePath = id.slice(root.length);
+          const regexp = new RegExp(`__vite__createHotContext\\("${relativePath}"\\)`, "g");
+          const replacement = `__vite__createHotContext("/@fs${id}")`;
+          return code.replace(regexp, replacement)
+        }
       }
     },
   };
